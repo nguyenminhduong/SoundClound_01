d9c350a0acbea6589da33c6695ada01c3b68b753
Nguyen Minh Duong <nguyen.minh.duong@framgia.com>
2016-12-26 15:35:09 +0700

    Custom callback api

---
+++ app/src/main/java/com/example/framgia/soundclound_01/service/API.java
@@ -0,0 +1,32 @@
+package com.example.framgia.soundclound_01.service;
+
+import com.example.framgia.soundclound_01.data.model.AudioResult;
+import com.example.framgia.soundclound_01.data.model.SearchAudioResult;
+
+import java.util.Map;
+
+import retrofit2.Callback;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+import static com.example.framgia.soundclound_01.utils.Const.APIConst.BaseURL.BASE_URL_SOUNDCLOUD;
+
+public abstract class API {
+    private static final APIServices clientSoundCloud = new Retrofit.Builder()
+        .baseUrl(BASE_URL_SOUNDCLOUD)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+        .create(APIServices.class);
+
+    public static void getAudio(Map<String, String> params,
+                                Callback<AudioResult> callback) {
+        clientSoundCloud.getAudio(params)
+            .enqueue(callback);
+    }
+
+    public static void getSearchingAudio(Map<String, String> params,
+                                         Callback<SearchAudioResult> callback) {
+        clientSoundCloud.getSearChingAudio(params)
+            .enqueue(callback);
+    }
+}
---
+++ app/src/main/java/com/example/framgia/soundclound_01/utils/Const.java
@@ -0,0 +1,27 @@
+package com.example.framgia.soundclound_01.utils;
+
+public class Const {
+    public class APIConst {
+        public class BaseURL {
+            public static final String BASE_URL_SOUNDCLOUD = "https://api-v2.soundcloud.com/";
+        }
+
+        public class Param {
+            public static final String PARAM_CLIENT_ID = "client_id";
+            public static final String PARAM_KIND_AUDIO = "kind";
+            public static final String PARAM_GENRE_AUDIO = "genre";
+            public static final String PARAM_QUERY = "q";
+            public static final String PARAM_LIMIT = "limit";
+        }
+
+        public class Path {
+            public static final String PATH_AUDIO =
+                "charts";
+            public static final String PATH_SEARCH_AUDIO = "search";
+        }
+
+        public class APIKey {
+            public static final String API_KEY_SOUNDCLOUD = "08f79801a998c381762ec5b15e4914d5";
+        }
+    }
+}
